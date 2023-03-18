81dc0fba020e14e1fd2e0b54d12b8cc7a312f486
Valentin Obst <valentin.obst@fkie.fraunhofer.de>
2023-03-18 12:56:11 +0100

    better validation of extracted BTF

--- src/plugins/btf_extract.py
+++ src/plugins/btf_extract.py
@@ -1,8 +1,11 @@
 import logging
+import tempfile
 from collections import namedtuple
 from struct import unpack
 from typing import List, ByteString, Iterable, Tuple
 from hashlib import md5
+from subprocess import run, DEVNULL, CalledProcessError
+import re
 
 from volatility3.framework import (
     interfaces,
@@ -31,6 +34,7 @@ BtfHeader = namedtuple(
     ],
 )
 
+
 class BtfExtract(interfaces.plugins.PluginInterface):
     """Extracts .BTF section(s) from the dump."""
 
@@ -49,10 +53,7 @@ class BtfExtract(interfaces.plugins.PluginInterface):
         ]
 
     @classmethod
-    def _probably_valid_btf(
-        cls,
-        hdr: BtfHeader
-    ) -> bool:
+    def _probably_valid_btf(cls, hdr: BtfHeader) -> bool:
         if (
             hdr.version != 1
             or hdr.flags != 0
@@ -63,18 +64,56 @@ class BtfExtract(interfaces.plugins.PluginInterface):
             or hdr.str_len == 0
         ):
             return False
-        if hdr.type_off >= hdr.str_off or hdr.type_off + hdr.type_len > hdr.str_off:
+        if (
+            hdr.type_off >= hdr.str_off
+            or hdr.type_off + hdr.type_len > hdr.str_off
+        ):
             return False
         if hdr.hdr_len + hdr.str_off + hdr.str_len > 2**29:
             return False
         return True
 
     @classmethod
-    def _probably_kernel_btf(
-        cls,
-        hdr: BtfHeader
-    ) -> bool:
+    def _likely_valid_btf(cls, btf: bytes) -> bool:
+        with tempfile.NamedTemporaryFile() as tmp:
+            tmp.write(btf)
+            try:
+                run(
+                    ["bpftool", "btf", "dump", "file", f"{tmp.name}"],
+                    stdout=DEVNULL,
+                    stderr=DEVNULL,
+                    timeout=10,
+                    check=True,
+                )
+                run(
+                    ["pahole", "-F", "btf", f"{tmp.name}"],
+                    stdout=DEVNULL,
+                    stderr=DEVNULL,
+                    timeout=10,
+                    check=True,
+                )
+            except CalledProcessError as E:
+                vollog.info(
+                    f"BTF cannot be parsed by cli tools: {E.cmd} {E.stderr}"
+                )
+                return False
+            return True
+
+    @classmethod
+    def _probably_kernel_btf(cls, hdr: BtfHeader) -> bool:
         return hdr.hdr_len + hdr.str_off + hdr.str_len > 2**20
+
+    @classmethod
+    def _likely_kernel_btf(cls, btf: bytes) -> bool:
+        kernel_strs = re.compile(
+            b"(__sys_setuid|__sys_setsockopt|__sys_bpf)"
+        )
+        if not re.search(kernel_strs, btf):
+            vollog.info(
+                f"BTF does not contain characteristic kernel strings"
+            )
+            return False
+        return True
 
     @classmethod
     def btf_extract(
@@ -84,7 +123,8 @@ class BtfExtract(interfaces.plugins.PluginInterface):
         kernel: bool = True,
     ) -> Iterable[Tuple[format_hints.Hex, bytes]]:
         """Extracts .BTF section(s) from the dump.
-        kernel: only extract BTF of vmlinux image
+        kernel: attempt to only extract BTF of vmlinux image (we also
+            find BTF of kernel modules or BPF objects)
         """
         layer = context.layers[layer_name]
         vollog.info(f"{layer.metadata.architecture=}")
@@ -99,11 +139,15 @@ class BtfExtract(interfaces.plugins.PluginInterface):
                 continue
             if kernel and not cls._probably_kernel_btf(hdr):
                 continue
-            vollog.info(f"{offset=}")
-            vollog.info(f"{hdr=}")
-            yield format_hints.Hex(offset), layer.read(
+            btf = layer.read(
                 offset, hdr.hdr_len + hdr.str_off + hdr.str_len
             )
+            if not cls._likely_valid_btf(btf):
+                continue
+            if kernel and not cls._likely_kernel_btf(btf):
+                continue
+            vollog.info(f"Found likely valid BTF: {offset=} {hdr=}")
+            yield format_hints.Hex(offset), btf
 
     def _generator(self):
         layer = self.context.layers[self.config["primary"]]
@@ -115,9 +159,16 @@ class BtfExtract(interfaces.plugins.PluginInterface):
             h = md5()
             h.update(btf_section)
 
-            filename = "_".join([hex(offset), h.digest().hex(), str(
-                self.context.layers["base_layer"].location
-            ).split("/")[-1] + ".btf"])
+            filename = "_".join(
+                [
+                    hex(offset),
+                    h.digest().hex(),
+                    str(
+                        self.context.layers["base_layer"].location
+                    ).split("/")[-1]
+                    + ".btf",
+                ]
+            )
             with self.open(filename) as f:
                 f.write(btf_section)
 
