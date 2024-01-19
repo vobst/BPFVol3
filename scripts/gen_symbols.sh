50ad154134caa329a7956d7c87900eee914fd522
Valentin Obst <valentin.obst@fkie.fraunhofer.de>
2024-01-19 12:20:00 +0100

    scripts/symbols: also gen symbols from btf

--- scripts/gen_symbols.sh
+++ scripts/gen_symbols.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-set -xeuo pipefail
+set -euo pipefail
+
+DOCKER_CMD="sudo -E docker"
 
 if [[ $# -ne 2 ]]; then
     echo "Generate ISF file" >&2
@@ -11,19 +13,33 @@ fi
 kernel_path="$1"
 kernel_name="$(basename ${kernel_path})"
 systemmap_path="$2"
-kernelhash=$(md5sum ${kernel_path} | rg '([0-9a-f]+?) ' -r '$1' -o | head -c 5 || true)
+kernelhash=$(sha256sum ${kernel_path} | rg '([0-9a-f]+?) ' -r '$1' -o | head -c 5 || true)
+maphash=$(sha256sum ${systemmap_path} | rg '([0-9a-f]+?) ' -r '$1' -o | head -c 5 || true)
 
-mv ${kernel_path} "io/kernels/${kernel_name}-${kernelhash}.elf"
-mv ${systemmap_path} "io/kernels/${kernel_name}-${kernelhash}.map"
+kernel="io/kernels/${kernel_name}-${kernelhash}.elf"
+system_map="io/kernels/${kernel_name}-${kernelhash}-${maphash}.map"
+symbols="io/symbols/${kernel_name}-${kernelhash}-${maphash}.btf2json.isf.json"
 
-kernel="/io/kernels/${kernel_name}-${kernelhash}.elf"
-system_map="/io/kernels/${kernel_name}-${kernelhash}.map"
-symbols="/io/symbols/${kernel_name}-${kernelhash}.isf.json"
+mv "${kernel_path}" "${kernel}"
+mv "${systemmap_path}" "${system_map}"
+
+btf2json \
+    --btf "io/kernels/${kernel_name}-${kernelhash}.elf" \
+    --map "io/kernels/${kernel_name}-${kernelhash}-${maphash}.map" |
+    tee "$symbols"
+
+./scripts/validate_schema.py \
+    "$symbols" \
+    "./volatility3/volatility3/schemas/schema-6.2.0.json"
+
+kernel="/${kernel}"
+system_map="/${system_map}"
+symbols="/io/symbols/${kernel_name}-${kernelhash}-${maphash}.dwarf2json.isf.json"
 d2j="/opt/vol/dwarf2json/dwarf2json"
 
 echo "Generating ISF file, this may take a while"
 
-docker run \
+$DOCKER_CMD run \
     --name "ISF_${kernel_name}_${kernelhash}" \
     --rm \
     -i \
